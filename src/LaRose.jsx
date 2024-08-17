7567be32ef07a687610c195ecdd3b00318ed1585
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-08-18 02:44:20 +0300

    Add files via upload

--- src/LaRose.jsx
+++ src/LaRose.jsx
@@ -1659,50 +1659,125 @@ export function LaRoseText({ children, fontSize = 2, fontWeight = 600, gradientT
     </>
   );
 }
-export default function RoseBox({ children, edit = {}, background = "", RoseName = "", RoseID = "", autoLayout = false, shadow = false, aspectRatio = null, atHover = false, grid = false, flex = false }) {
-  const layoutStyle = autoLayout
-    ? { padding: '10px', margin: '0 auto', overflow: 'hidden' }
-    : {};
-  const shadowStyle = shadow
-    ? { boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }
-    : {};
-  const aspectStyle = aspectRatio
-    ? { aspectRatio }
-    : {};
 
-  const DisplaylayoutStyle = grid
-    ? { display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))' }
-    : flex
-      ? { display: 'flex', justifyContent: 'space-between', alignItems: 'center' }
-      : {};
-  const backgroundStyle = background
-    ? { background }
-    : {};
+export default function RoseBox({
+    children,
+    edit = {},
+    background = "",
+    RoseName = "",
+    RoseID = "",
+    autoLayout = false,
+    shadow = false,
+    shadowColor = "black",
+    aspectRatio = null,
+    atHover = false,
+    grid = false,
+    flex = false,
+    scaleAnimate = false,
+    childrenVisible = true,
+    onClick,
+    ariaLabel,
+    role,
+    lazy = false,
+    AutoHandling = false,
+    outlineOffset = 0
+}) {
+    const [isLoaded, setIsLoaded] = useState(!lazy);
 
-  // Apply the hover class conditionally
-  const hoverClass = atHover ? 'rosebox-hover' : '';
+    useEffect(() => {
+        if (lazy) {
+            const timer = setTimeout(() => {
+                setIsLoaded(true);
+            }, 200); // Adjust the delay as needed
+            return () => clearTimeout(timer);
+        }
+    }, [lazy]);
 
-  return (
-    <>
-      <style>{`
-          .rosebox-hover {
-      transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out;
-  }
+    const layoutStyle = autoLayout
+        ? { padding: '0.8rem', margin: '0 auto', overflow: 'hidden' }
+        : {};
+    const shadowStyle = shadow
+        ? { boxShadow: `0px 0px 9px 1px ${shadowColor}` }
+        : {};
+    const aspectStyle = aspectRatio
+        ? { aspectRatio }
+        : {};
 
-  .rosebox-hover:hover {
-      transform: scale(1.05);
-      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-      background-color: rgba(240, 240, 240, 0.8);
-  }
+    const displayLayoutStyle = grid
+        ? { display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))' }
+        : flex
+            ? { display: 'flex', justifyContent: 'space-between', alignItems: 'center' }
+            : {};
+    const backgroundStyle = background
+        ? { background }
+        : {};
 
-      `}</style>
-      <div
-        id={RoseID}
-        className={`${RoseName} ${hoverClass}`}
-        style={{ ...edit, ...layoutStyle, ...shadowStyle, ...aspectStyle, ...DisplaylayoutStyle, ...backgroundStyle }}
-      >
-        {children}
-      </div>
-    </>
-  );
+    const autoHandleStyle = AutoHandling ? {
+        background: "#1b85db",
+        height: "3rem",
+        minWidth: "11rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid #1b85db",
+        borderRadius: "7px",
+        fontWeight: "bold",
+        boxShadow: "#1b85db 0px 50px 100px -20px, #1b85db 0px 30px 60px -30px, #1b85db 0px -2px 6px 0px inset",
+        outlineOffset: `${outlineOffset}`,
+        outlineStyle: "solid",
+        outlineColor: "#1b85db",
+        outlineWidth: "2px",
+        cursor: "pointer"
+    } : {};
+
+    // Apply the hover class conditionally
+    const hoverClass = atHover ? 'rosebox-hover' : '';
+
+    const handleClick = () => {
+        if (typeof onClick === "function") {
+            onClick();
+        }
+    }
+
+    return (
+        <>
+            <style>{`
+                .rosebox-hover {
+                    transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out;
+                }
+
+                .rosebox-hover:hover {
+                    transform: scale(1.05);
+                    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+                    background-color: rgba(240, 240, 240, 0.8);
+                }
+
+                .rosebox-scale-animate {
+                    animation: scale-down 0.6s ease-out forwards;
+                }
+
+                @keyframes scale-down {
+                    0% {
+                        transform: scale(2);
+                    }
+                    100% {
+                        transform: scale(0.9);
+                    }
+                }
+            `}</style>
+            {childrenVisible && (
+                <div
+                    aria-label={ariaLabel}
+                    role={role}
+                    onClick={handleClick}
+                    id={RoseID}
+                    className={`${RoseName} ${hoverClass} ${scaleAnimate ? 'rosebox-scale-animate' : ''}`}
+                    style={{ ...edit, ...layoutStyle, ...shadowStyle, ...aspectStyle, ...displayLayoutStyle, ...backgroundStyle, ...autoHandleStyle }}
+                >
+                    {isLoaded ? children : <div>Loading...</div>}
+                </div>
+            )}
+        </>
+    );
 }
+
--- src/Tabs/Default.jsx
+++ src/Tabs/Default.jsx
@@ -8,28 +8,13 @@ function Default() {
         <>
             <div className="App">
                 <BgSvg />
-                <div className="sliderStyle">
-                    <Slider>
-                        <div className="slide">
-                            <div className="alert">
-                                <Ak_Alert title={"Easy to customize Code"} time={Infinity} />
-                                <hr />
-                                <div style={{ cursor: "pointer" }} onClick={() => { window.open("/Home", "_self") }}><Ak_Alert title={"Go To Home Page"} time={Infinity} /></div>
-                            </div>
-                        </div>
-                        <div className="slide">
-                            <div className="alert2">
-                                <Ak_Alert title={"Easy to Code"} time={Infinity} />
-                            </div>
-                        </div>
-                    </Slider>
-                </div>
 
-                {/* Register Component */}
-                <div className="register">
-                    <Register Name={"Example@gmail.com"} Pass={"123456"} />
-                </div>
 
+
+
+                <RoseBox AutoHandling outlineOffset={"5px"}>
+                    Hello World
+                </RoseBox>
 
 
 
