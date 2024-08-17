e707883c251bdd8b55a65dbd45920b21c432da69
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-08-17 20:46:04 +0300

    Add files via upload

--- src/LaRose.jsx
+++ src/LaRose.jsx
@@ -967,7 +967,6 @@ export function Loader() {
 
   )
 }
-
 export function Slider({ children, autoplay = false, autoplayInterval = 3000 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -1657,6 +1656,53 @@ export function LaRoseText({ children, fontSize = 2, fontWeight = 600, gradientT
         }
       `}</style>
       <div className="title">{children}</div>
+    </>
+  );
+}
+export default function RoseBox({ children, edit = {}, background = "", RoseName = "", RoseID = "", autoLayout = false, shadow = false, aspectRatio = null, atHover = false, grid = false, flex = false }) {
+  const layoutStyle = autoLayout
+    ? { padding: '10px', margin: '0 auto', overflow: 'hidden' }
+    : {};
+  const shadowStyle = shadow
+    ? { boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }
+    : {};
+  const aspectStyle = aspectRatio
+    ? { aspectRatio }
+    : {};
+
+  const DisplaylayoutStyle = grid
+    ? { display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))' }
+    : flex
+      ? { display: 'flex', justifyContent: 'space-between', alignItems: 'center' }
+      : {};
+  const backgroundStyle = background
+    ? { background }
+    : {};
+
+  // Apply the hover class conditionally
+  const hoverClass = atHover ? 'rosebox-hover' : '';
+
+  return (
+    <>
+      <style>{`
+          .rosebox-hover {
+      transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, background-color 0.3s ease-in-out;
+  }
+
+  .rosebox-hover:hover {
+      transform: scale(1.05);
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+      background-color: rgba(240, 240, 240, 0.8);
+  }
+
+      `}</style>
+      <div
+        id={RoseID}
+        className={`${RoseName} ${hoverClass}`}
+        style={{ ...edit, ...layoutStyle, ...shadowStyle, ...aspectStyle, ...DisplaylayoutStyle, ...backgroundStyle }}
+      >
+        {children}
+      </div>
     </>
   );
 }
