7f84ade9b401fbcce15882a52f9abb12f5bd97cf
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-09-04 05:29:19 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { RoseBox,WaveText } from "./LaRose";
+export default function App() {
+  return (
+    <RoseBox autoLayout AutoHandling>
+      <WaveText>
+        LaRose App
+      </WaveText>
+    </RoseBox>
+  );
+}
