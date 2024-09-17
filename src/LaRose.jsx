12c723ca0e220ca3d37cf1369177b97c3a71ba1b
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-09-17 19:05:38 +0300

    Update LaRose.jsx

--- src/LaRose.jsx
+++ src/LaRose.jsx
@@ -2677,3 +2677,15 @@ export default function TestManegar({ children }) {
     </div>
   );
 }
+export const useRenderTime = () => {
+  const [renderTime, setRenderTime] = useState(null);
+  useEffect(() => {
+    const startTime = performance.now();
+    // Perform the measurement after the next paint
+    requestAnimationFrame(() => {
+      const endTime = performance.now();
+      setRenderTime(endTime - startTime); // Calculate render time in milliseconds
+    });
+  }, []); // Run only once on mount
+  return renderTime;
+};
