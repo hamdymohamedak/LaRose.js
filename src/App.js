8a684bf3b36874d7a57896f515d8da71f94d64ea
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-10-01 05:12:46 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {
+  RoseBox,
+  RandomAnimate,
+  WaveText,
+  useRenderTime,
+  useLocalStorage,
+  BlockUser,
+  ShinyText,
+  ShinyButton,
+} from "larose-js";
+import "./App.css";
+export default function App() {
+  const renderTime = useRenderTime();
+
+  let open_docs = () => {
+    window.open("https://larose.vercel.app");
+  };
+
+  return (
+    <RoseBox RoseName="App">
+      <WaveText>let's Start Development</WaveText>
+      <RandomAnimate edit={{}}>Hello world</RandomAnimate>
+      <ShinyButton ShinyButtonEvent={open_docs}>
+        <ShinyText edit={{ color: "white" }}>Learn LaRose</ShinyText>
+      </ShinyButton>
+      <div>
+        {renderTime !== null && (
+          <p>Render time Speed: {renderTime.toFixed(2)} ms</p>
+        )}
+      </div>
+    </RoseBox>
+  );
+}
